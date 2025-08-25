10c2f04d5f45925526691c7193cf777a8c31dfc7
yasir <hellosakib2004@gmail.com>
2025-08-26 04:58:11 +0600

    signup,login,landing page UI

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Documents\Django\deal_book\venv
+set VIRTUAL_ENV=D:\DIAMOND AI\Tashin_Dealbook\DealBook\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
