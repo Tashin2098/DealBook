a7bf952ada055fe0ad3c218359a5030f320954f6
Tashin2098 <md.tashin.rahman@g.bracu.ac.bd>
2025-09-06 04:01:36 +0600

    Prepare Django for Vercel

--- dealbook/settings.py
+++ dealbook/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hi^p9@9a&l@%%qt9)z+m4jm&0#76i7jzntmpd@q0&ge8phx(jw'
+
+SECRET_KEY = os.getenv("django-insecure-hi^p9@9a&l@%%qt9)z+m4jm&0#76i7jzntmpd@q0&ge8phx(jw'", 'change-this-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -90,10 +92,11 @@ WSGI_APPLICATION = 'dealbook.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",  # fallback for local
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 
@@ -140,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / 'core' / 'static',
 ]
